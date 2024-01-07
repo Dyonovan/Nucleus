991240c1481d6863a52bc615a8b5ed94c36d4393
pauljoda <pauldavis101@gmail.com>
2024-01-07 16:46:02 -0600

    Fix server register issue

--- src/main/java/com/pauljoda/nucleus/common/CommonEvents.java
+++ src/main/java/com/pauljoda/nucleus/common/CommonEvents.java
@@ -6,13 +6,13 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
-@Mod.EventBusSubscriber(modid = Nucleus.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.DEDICATED_SERVER)
+@Mod.EventBusSubscriber(modid = Nucleus.MODID, value = Dist.DEDICATED_SERVER)
 public class CommonEvents {
 
     /**
      * This method is called on initialization.
      */
     @SubscribeEvent
-    public void ServerLoad(ServerStartingEvent event) {
+    public static void ServerLoad(ServerStartingEvent event) {
     }
 }
