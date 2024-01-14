77c8f7e3508f6be3d39258c674657abf5863a7fa
pauljoda <pauldavis101@gmail.com>
2024-01-14 15:26:35 -0600

    Refactor InventoryHandler class and add Savable interface
    Simplified and refactored the InventoryHandler class by moving common code to a new InventoryHolder class which handles inventory operations. Also, introduced a new Savable interface representing objects capable of saving and loading data. The changes help improve code readability and maintainability.

--- src/main/java/com/pauljoda/nucleus/common/blocks/entity/item/InventoryHandler.java
+++ src/main/java/com/pauljoda/nucleus/common/blocks/entity/item/InventoryHandler.java
@@ -1,5 +1,6 @@
 package com.pauljoda.nucleus.common.blocks.entity.item;
 
+import com.pauljoda.nucleus.capabilities.InventoryHolder;
 import com.pauljoda.nucleus.common.blocks.entity.Syncable;
 import com.pauljoda.nucleus.common.container.IInventoryCallback;
 import net.minecraft.core.BlockPos;
@@ -31,14 +32,22 @@ import java.util.List;
  */
 public abstract class InventoryHandler extends Syncable implements IItemHandlerModifiable {
 
-    // Variables
-    // A list to hold all callback objects
-    private List<IInventoryCallback> callBacks = new ArrayList<>();
-    // List of Inventory contents
-    public NonNullList<ItemStack> inventoryContents = NonNullList.withSize(getInventorySize(), ItemStack.EMPTY);
+    private InventoryHolder inventory;
 
     public InventoryHandler(BlockEntityType<?> tileEntityTypeIn, BlockPos pos, BlockState state) {
         super(tileEntityTypeIn, pos, state);
+
+        inventory = new InventoryHolder() {
+            @Override
+            protected int getInventorySize() {
+                return InventoryHandler.this.getInventorySize();
+            }
+
+            @Override
+            protected boolean isItemValidForSlot(int index, ItemStack stack) {
+                return InventoryHandler.this.isItemValidForSlot(index, stack);
+            }
+        };
     }
 
     /*******************************************************************************************************************
@@ -62,53 +71,16 @@ public abstract class InventoryHandler extends Syncable implements IItemHandlerM
     protected abstract boolean isItemValidForSlot(int index, ItemStack stack);
 
     /*******************************************************************************************************************
-     * InventoryHandler                                                                                                *
+     * Capabilities                                                                                                    *
      *******************************************************************************************************************/
 
     /**
-     * Add a callback to this inventory
+     * Retrieves the item capability of the inventory.
      *
-     * @param iInventoryCallback The callback you wish to add
-     * @return This object, to enable chaining
+     * @return The item capability of the inventory.
      */
-    public InventoryHandler addCallback(IInventoryCallback iInventoryCallback) {
-        callBacks.add(iInventoryCallback);
-        return this;
-    }
-
-    /**
-     * Called when the inventory has a change
-     *
-     * @param slot The slot that changed
-     */
-    protected void onInventoryChanged(int slot) {
-        callBacks.forEach((IInventoryCallback callback) -> callback.onInventoryChanged(this, slot));
-    }
-
-    /**
-     * Used to copy from an existing inventory
-     *
-     * @param inventory The inventory to copy from
-     */
-    public void copyFrom(IItemHandler inventory) {
-        for (int i = 0; i < inventory.getSlots(); i++) {
-            if (i < inventoryContents.size()) {
-                ItemStack stack = inventory.getStackInSlot(i);
-                if (!stack.isEmpty())
-                    inventoryContents.set(i, stack.copy());
-                else
-                    inventoryContents.set(i, ItemStack.EMPTY);
-            }
-        }
-    }
-
-    /**
-     * Makes sure this slot is within our range
-     *
-     * @param slot Which slot
-     */
-    protected boolean isValidSlot(int slot) {
-        return slot >= 0 && slot <= inventoryContents.size();
+    public IItemHandlerModifiable getItemCapability() {
+        return inventory;
     }
 
     /*******************************************************************************************************************
@@ -123,22 +95,7 @@ public abstract class InventoryHandler extends Syncable implements IItemHandlerM
     @Override
     public void load(CompoundTag compound) {
         super.load(compound);
-
-        ContainerHelper.loadAllItems(compound, inventoryContents);
-
-        ListTag nbttaglist = new ListTag();
-
-        for (int i = 0; i < inventoryContents.size(); ++i) {
-            ItemStack itemstack = inventoryContents.get(i);
-            CompoundTag nbttagcompound = new CompoundTag();
-            nbttagcompound.putByte("Slot", (byte) i);
-            itemstack.save(nbttagcompound);
-            nbttaglist.add(nbttagcompound);
-        }
-
-        if (!nbttaglist.isEmpty()) {
-            compound.put("Items", nbttaglist);
-        }
+        inventory.load(compound);
     }
 
     /**
@@ -149,214 +106,6 @@ public abstract class InventoryHandler extends Syncable implements IItemHandlerM
     @Override
     public void saveAdditional(CompoundTag compound) {
         super.saveAdditional(compound);
-        ContainerHelper.saveAllItems(compound, inventoryContents);
-    }
-
-    /*******************************************************************************************************************
-     * IItemHandlerModifiable                                                                                          *
-     *******************************************************************************************************************/
-
-    /**
-     * Overrides the stack in the given slot. This method is used by the
-     * standard Forge helper methods and classes. It is not intended for
-     * general use by other mods, and the handler may throw an error if it
-     * is called unexpectedly.
-     *
-     * @param slot  Slot to modify
-     * @param stack ItemStack to set slot to (may be null)
-     * @throws RuntimeException if the handler is called in a way that the handler
-     *                          was not expecting.
-     **/
-    @Override
-    public void setStackInSlot(int slot, ItemStack stack) {
-        if (!isValidSlot(slot))
-            return;
-        if (ItemStack.isSameItemSameTags(this.inventoryContents.get(slot), stack))
-            return;
-        this.inventoryContents.set(slot, stack);
-        onInventoryChanged(slot);
-    }
-
-    /*******************************************************************************************************************
-     * IItemHandler                                                                                                    *
-     *******************************************************************************************************************/
-
-    /**
-     * Returns the number of slots available
-     *
-     * @return The number of slots available
-     **/
-    @Override
-    public int getSlots() {
-        return inventoryContents.size();
-    }
-
-    /**
-     * Returns the ItemStack in a given slot.
-     * <p>
-     * The result's stack size may be greater than the itemstacks max size.
-     * <p>
-     * If the result is null, then the slot is empty.
-     * If the result is not null but the stack size is zero, then it represents
-     * an empty slot that will only accept* a specific itemstack.
-     * <p>
-     * IMPORTANT: This ItemStack MUST NOT be modified. This method is not for
-     * altering an inventories contents. Any implementers who are able to detect
-     * modification through this method should throw an exception.
-     * p
-     * SERIOUSLY: DO NOT MODIFY THE RETURNED ITEMSTACK
-     *
-     * @param slot Slot to query
-     * @return ItemStack in given slot. May not be null.
-     **/
-    @Override
-    @Nonnull
-    public ItemStack getStackInSlot(int slot) {
-        if (!isValidSlot(slot))
-            return ItemStack.EMPTY;
-        return inventoryContents.get(slot);
-    }
-
-    /**
-     * Inserts an ItemStack into the given slot and return the remainder.
-     * The ItemStack should not be modified in this function!
-     * Note: This behaviour is subtly different from IFluidHandlers.fill()
-     *
-     * @param slot     Slot to insert into.
-     * @param stack    ItemStack to insert.
-     * @param simulate If true, the insertion is only simulated
-     * @return The remaining ItemStack that was not inserted (if the entire stack is accepted, then return null).
-     * May be the same as the input ItemStack if unchanged, otherwise a new ItemStack.
-     **/
-    @Nonnull
-    @Override
-    public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-        if (!isInputSlot(slot) || !isItemValidForSlot(slot, stack))
-            return stack;
-
-        if (stack.isEmpty() || !isValidSlot(slot))
-            return stack;
-
-        ItemStack existing = this.inventoryContents.get(slot);
-
-        int limit = getSlotLimit(slot);
-
-        if (!existing.isEmpty()) {
-            if (!ItemHandlerHelper.canItemStacksStack(stack, existing))
-                return stack;
-
-            limit -= existing.getCount();
-        }
-
-        if (limit <= 0)
-            return stack;
-
-        boolean reachedLimit = stack.getCount() > limit;
-
-        if (!simulate) {
-            if (existing.isEmpty()) {
-                this.inventoryContents.set(slot, reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, limit) : stack);
-            } else {
-                existing.setCount(existing.getCount() + (reachedLimit ? limit : stack.getCount()));
-            }
-            onInventoryChanged(slot);
-        }
-
-        return reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - limit) : ItemStack.EMPTY;
-    }
-
-    /**
-     * Checks if the specified slot is an input slot.
-     *
-     * @param slot The slot number to check.
-     * @return true if the slot is an input slot, false otherwise.
-     */
-    public boolean isInputSlot(int slot) {
-        return true;
-    }
-
-    /**
-     * Extracts an ItemStack from the given slot. The returned value must be null
-     * if nothing is extracted, otherwise it's stack size must not be greater than amount or the
-     * itemstacks getMaxStackSize().
-     *
-     * @param slot     Slot to extract from.
-     * @param amount   Amount to extract (may be greater than the current stacks max limit)
-     * @param simulate If true, the extraction is only simulated
-     * @return ItemStack extracted from the slot, must be null, if nothing can be extracted
-     **/
-    @Nonnull
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        if (!isOutputSlot(slot) || amount == 0)
-            return ItemStack.EMPTY;
-
-        if (!isValidSlot(slot))
-            return ItemStack.EMPTY;
-        ItemStack existing = this.inventoryContents.get(slot);
-
-        if (existing.isEmpty())
-            return ItemStack.EMPTY;
-
-        int toExtract = Math.min(amount, existing.getMaxStackSize());
-
-        if (existing.getCount() <= toExtract) {
-            if (!simulate) {
-                this.inventoryContents.set(slot, ItemStack.EMPTY);
-                onInventoryChanged(slot);
-            }
-            return existing;
-        } else {
-            if (!simulate) {
-                this.inventoryContents.set(slot, ItemHandlerHelper.copyStackWithSize(existing, existing.getCount() - toExtract));
-                onInventoryChanged(slot);
-            }
-
-            return ItemHandlerHelper.copyStackWithSize(existing, toExtract);
-        }
-    }
-
-    /**
-     * Checks if the current slot is an output slot.
-     *
-     * @return true if the current slot is an output slot, false otherwise.
-     */
-    public boolean isOutputSlot(int slot) {
-        return true;
-    }
-
-    /**
-     * Retrieves the maximum stack size allowed to exist in the given slot.
-     *
-     * @param slot Slot to query.
-     * @return The maximum stack size allowed in the slot.
-     */
-    @Override
-    public int getSlotLimit(int slot) {
-        return 64;
-    }
-
-    /**
-     * <p>
-     * This function re-implements the vanilla function.
-     * It should be used instead of simulated insertions in cases where the contents and state of the inventory are
-     * irrelevant, mainly for the purpose of automation and logic (for instance, testing if a minecart can wait
-     * to deposit its items into a full inventory, or if the items in the minecart can never be placed into the
-     * inventory and should move on).
-     * </p>
-     * <ul>
-     * <li>isItemValid is false when insertion of the item is never valid.</li>
-     * <li>When isItemValid is true, no assumptions can be made and insertion must be simulated case-by-case.</li>
-     * <li>The actual items in the inventory, its fullness, or any other state are <strong>not</strong> considered by isItemValid.</li>
-     * </ul>
-     *
-     * @param slot  Slot to query for validity
-     * @param stack Stack to test with for validity
-     * @return true if the slot can insert the ItemStack, not considering the current state of the inventory.
-     * false if the slot can never insert the ItemStack in any situation.
-     */
-    @Override
-    public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-        return isItemValidForSlot(slot, stack);
+        inventory.save(compound);
     }
 }
--- src/main/java/com/pauljoda/nucleus/common/items/InventoryHandlerItem.java
+++ src/main/java/com/pauljoda/nucleus/common/items/InventoryHandlerItem.java
@@ -1,5 +1,6 @@
 package com.pauljoda.nucleus.common.items;
 
+import com.pauljoda.nucleus.capabilities.InventoryHolder;
 import com.pauljoda.nucleus.common.container.IInventoryCallback;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
@@ -29,10 +30,8 @@ public abstract class InventoryHandlerItem implements IItemHandlerModifiable {
 
     // Variables
     private ItemStack heldStack;
-    // A list to hold all callback objects
-    private List<IInventoryCallback> callBacks = new ArrayList<>();
-    // List of Inventory contents
-    public NonNullList<ItemStack> inventoryContents = NonNullList.withSize(getInventorySize(), ItemStack.EMPTY);
+
+    private InventoryHolder inventory;
 
     /**
      * Creates a handler with given stack
@@ -41,7 +40,20 @@ public abstract class InventoryHandlerItem implements IItemHandlerModifiable {
      */
     public InventoryHandlerItem(ItemStack stack, CompoundTag compound) {
         heldStack = stack;
-        readFromNBT(compound);
+
+        inventory = new InventoryHolder() {
+            @Override
+            protected int getInventorySize() {
+                return InventoryHandlerItem.this.getInventorySize()
+            }
+
+            @Override
+            protected boolean isItemValidForSlot(int index, ItemStack stack) {
+                return InventoryHandlerItem.this.isItemValidForSlot();
+            }
+        };
+
+        inventory.load(compound);
         checkStackTag();
     }
 
@@ -70,50 +82,12 @@ public abstract class InventoryHandlerItem implements IItemHandlerModifiable {
      *******************************************************************************************************************/
 
     /**
-     * Add a callback to this inventory
+     * Retrieves the item handler capability associated with the inventory handler.
      *
-     * @param iInventoryCallback The callback you wish to add
-     * @return This object, to enable chaining
+     * @return The item handler capability.
      */
-    public InventoryHandlerItem addCallback(IInventoryCallback iInventoryCallback) {
-        callBacks.add(iInventoryCallback);
-        return this;
-    }
-
-    /**
-     * Called when the inventory has a change
-     *
-     * @param slot The slot that changed
-     */
-    protected void onInventoryChanged(int slot) {
-        callBacks.forEach((IInventoryCallback callback) -> callback.onInventoryChanged(this, slot));
-        writeToNBT(heldStack.getTag());
-    }
-
-    /**
-     * Used to copy from an existing inventory
-     *
-     * @param inventory The inventory to copy from
-     */
-    public void copyFrom(IItemHandler inventory) {
-        for (int i = 0; i < inventory.getSlots(); i++) {
-            if (i < inventoryContents.size()) {
-                ItemStack stack = inventory.getStackInSlot(i);
-                if (!stack.isEmpty())
-                    inventoryContents.set(i, stack.copy());
-                else
-                    inventoryContents.set(i, ItemStack.EMPTY);
-            }
-        }
-    }
-
-    /**
-     * Makes sure this slot is within our range
-     *
-     * @param slot Which slot
-     */
-    protected boolean isValidSlot(int slot) {
-        return slot > 0 || slot <= inventoryContents.size();
+    public IItemHandlerModifiable getItemHandlerCapability() {
+        return inventory;
     }
 
     /**
@@ -123,194 +97,8 @@ public abstract class InventoryHandlerItem implements IItemHandlerModifiable {
         // Give the stack a tag
         if (!heldStack.hasTag()) {
             heldStack.setTag(new CompoundTag());
-            writeToNBT(heldStack.getTag());
+            inventory.save(heldStack.getTag());
         } else
-            readFromNBT(heldStack.getTag());
-    }
-
-    /**
-     * Used to save the inventory to an NBT tag
-     *
-     * @param compound The tag to save to
-     */
-    public CompoundTag writeToNBT(CompoundTag compound) {
-        ContainerHelper.saveAllItems(compound, inventoryContents);
-        return compound;
-    }
-
-    /**
-     * Used to read the inventory from an NBT tag compound
-     *
-     * @param compound The tag to read from
-     */
-    public void readFromNBT(CompoundTag compound) {
-        if (compound != null)
-            ContainerHelper.loadAllItems(compound, inventoryContents);
-    }
-
-    /*******************************************************************************************************************
-     * IItemHandlerModifiable                                                                                          *
-     *******************************************************************************************************************/
-
-    /**
-     * Overrides the stack in the given slot. This method is used by the
-     * standard Forge helper methods and classes. It is not intended for
-     * general use by other mods, and the handler may throw an error if it
-     * is called unexpectedly.
-     *
-     * @param slot  Slot to modify
-     * @param stack ItemStack to set slot to (may be null)
-     * @throws RuntimeException if the handler is called in a way that the handler
-     *                          was not expecting.
-     **/
-    @Override
-    public void setStackInSlot(int slot, ItemStack stack) {
-        if (!isValidSlot(slot))
-            return;
-        if (ItemStack.isSameItemSameTags(this.inventoryContents.get(slot), stack))
-            return;
-        this.inventoryContents.set(slot, stack);
-        onInventoryChanged(slot);
-    }
-
-    /*******************************************************************************************************************
-     * IItemHandler                                                                                                    *
-     *******************************************************************************************************************/
-
-    /**
-     * Returns the number of slots available
-     *
-     * @return The number of slots available
-     **/
-    @Override
-    public int getSlots() {
-        checkStackTag();
-        return inventoryContents.size();
-    }
-
-    /**
-     * Returns the ItemStack in a given slot.
-     * <p>
-     * The result's stack size may be greater than the itemstacks max size.
-     * <p>
-     * If the result is null, then the slot is empty.
-     * If the result is not null but the stack size is zero, then it represents
-     * an empty slot that will only accept* a specific itemstack.
-     * <p>
-     * IMPORTANT: This ItemStack MUST NOT be modified. This method is not for
-     * altering an inventories contents. Any implementers who are able to detect
-     * modification through this method should throw an exception.
-     * <p>
-     * SERIOUSLY: DO NOT MODIFY THE RETURNED ITEMSTACK
-     *
-     * @param slot Slot to query
-     * @return ItemStack in given slot. May not be null.
-     **/
-    @Override
-    @Nonnull
-    public ItemStack getStackInSlot(int slot) {
-        checkStackTag();
-        if (!isValidSlot(slot))
-            return ItemStack.EMPTY;
-        return inventoryContents.get(slot);
-    }
-
-    /**
-     * Inserts an ItemStack into the given slot and return the remainder.
-     * The ItemStack should not be modified in this function!
-     * Note: This behaviour is subtly different from IFluidHandlers.fill()
-     *
-     * @param slot     Slot to insert into.
-     * @param stack    ItemStack to insert.
-     * @param simulate If true, the insertion is only simulated
-     * @return The remaining ItemStack that was not inserted (if the entire stack is accepted, then return null).
-     * May be the same as the input ItemStack if unchanged, otherwise a new ItemStack.
-     **/
-    @Nonnull
-    @Override
-    public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-        checkStackTag();
-        if (isItemValidForSlot(slot, stack) || stack.isEmpty() || !isValidSlot(slot))
-            return stack;
-
-        ItemStack existing = this.inventoryContents.get(slot);
-
-        int limit = getSlotLimit(slot);
-
-        if (!existing.isEmpty()) {
-            if (!ItemHandlerHelper.canItemStacksStack(stack, existing))
-                return stack;
-
-            limit -= existing.getCount();
-        }
-
-        if (limit <= 0)
-            return stack;
-
-        boolean reachedLimit = stack.getCount() > limit;
-
-        if (!simulate) {
-            if (existing.isEmpty()) {
-                this.inventoryContents.set(slot, reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, limit) : stack);
-            } else {
-                existing.setCount(existing.getCount() + (reachedLimit ? limit : stack.getCount()));
-            }
-            onInventoryChanged(slot);
-        }
-
-        return reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - limit) : ItemStack.EMPTY;
-    }
-
-    /**
-     * Extracts an ItemStack from the given slot. The returned value must be null
-     * if nothing is extracted, otherwise it's stack size must not be greater than amount or the
-     * itemstacks getMaxStackSize().
-     *
-     * @param slot     Slot to extract from.
-     * @param amount   Amount to extract (may be greater than the current stacks max limit)
-     * @param simulate If true, the extraction is only simulated
-     * @return ItemStack extracted from the slot, must be null, if nothing can be extracted
-     **/
-    @Nonnull
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        checkStackTag();
-        if (amount == 0)
-            return ItemStack.EMPTY;
-
-        if (!isValidSlot(slot))
-            return ItemStack.EMPTY;
-        ItemStack existing = this.inventoryContents.get(slot);
-
-        if (existing.isEmpty())
-            return ItemStack.EMPTY;
-
-        int toExtract = Math.min(amount, existing.getMaxStackSize());
-
-        if (existing.getCount() <= toExtract) {
-            if (!simulate) {
-                this.inventoryContents.set(slot, ItemStack.EMPTY);
-                onInventoryChanged(slot);
-            }
-            return existing;
-        } else {
-            if (!simulate) {
-                this.inventoryContents.set(slot, ItemHandlerHelper.copyStackWithSize(existing, existing.getCount() - toExtract));
-                onInventoryChanged(slot);
-            }
-
-            return ItemHandlerHelper.copyStackWithSize(existing, toExtract);
-        }
-    }
-
-    /**
-     * Retrieves the maximum stack size allowed to exist in the given slot.
-     *
-     * @param slot Slot to query.
-     * @return The maximum stack size allowed in the slot.
-     */
-    @Override
-    public int getSlotLimit(int slot) {
-        return 64;
+            inventory.load(heldStack.getTag());
     }
 }
